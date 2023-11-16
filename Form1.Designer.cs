e82cb5102978ce6d9560e7dd4fb3e2727908dfbe
UnGatoprogramador <manuelrs2003@gmail.com>
2023-11-16 11:27:46 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,343 @@
+﻿namespace diosmio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.idEmpleado = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Dni = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Distrito = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Direccion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Sueldo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CreatedAt = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btCancelar = new System.Windows.Forms.Button();
+            this.btBorrar = new System.Windows.Forms.Button();
+            this.btGuardar = new System.Windows.Forms.Button();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.txtDistrito = new System.Windows.Forms.TextBox();
+            this.txtSueldo = new System.Windows.Forms.TextBox();
+            this.txtDni = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btBusqueda = new System.Windows.Forms.Button();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idEmpleado,
+            this.Nombre,
+            this.Dni,
+            this.Distrito,
+            this.Direccion,
+            this.Sueldo,
+            this.CreatedAt});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.dataGridView1.Location = new System.Drawing.Point(381, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(546, 450);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.DoubleClick += new System.EventHandler(this.dataGridView1_DoubleClick);
+            // 
+            // idEmpleado
+            // 
+            this.idEmpleado.DataPropertyName = "idEmpleado";
+            this.idEmpleado.HeaderText = "IdEmpleado";
+            this.idEmpleado.Name = "idEmpleado";
+            this.idEmpleado.ReadOnly = true;
+            this.idEmpleado.Visible = false;
+            // 
+            // Nombre
+            // 
+            this.Nombre.DataPropertyName = "Nombre";
+            this.Nombre.HeaderText = "nombre";
+            this.Nombre.Name = "Nombre";
+            this.Nombre.ReadOnly = true;
+            // 
+            // Dni
+            // 
+            this.Dni.DataPropertyName = "Dni";
+            this.Dni.HeaderText = "dni";
+            this.Dni.Name = "Dni";
+            this.Dni.ReadOnly = true;
+            // 
+            // Distrito
+            // 
+            this.Distrito.DataPropertyName = "Distrito";
+            this.Distrito.HeaderText = "distrito";
+            this.Distrito.Name = "Distrito";
+            this.Distrito.ReadOnly = true;
+            // 
+            // Direccion
+            // 
+            this.Direccion.DataPropertyName = "Direccion";
+            this.Direccion.HeaderText = "direccion";
+            this.Direccion.Name = "Direccion";
+            this.Direccion.ReadOnly = true;
+            // 
+            // Sueldo
+            // 
+            this.Sueldo.DataPropertyName = "Sueldo";
+            this.Sueldo.HeaderText = "sueldo";
+            this.Sueldo.Name = "Sueldo";
+            this.Sueldo.ReadOnly = true;
+            // 
+            // CreatedAt
+            // 
+            this.CreatedAt.DataPropertyName = "CreatedAt";
+            this.CreatedAt.HeaderText = "CA";
+            this.CreatedAt.Name = "CreatedAt";
+            this.CreatedAt.ReadOnly = true;
+            this.CreatedAt.Visible = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btCancelar);
+            this.groupBox1.Controls.Add(this.btBorrar);
+            this.groupBox1.Controls.Add(this.btGuardar);
+            this.groupBox1.Controls.Add(this.txtDireccion);
+            this.groupBox1.Controls.Add(this.txtDistrito);
+            this.groupBox1.Controls.Add(this.txtSueldo);
+            this.groupBox1.Controls.Add(this.txtDni);
+            this.groupBox1.Controls.Add(this.txtNombre);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(350, 374);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos de Empleado";
+            // 
+            // btCancelar
+            // 
+            this.btCancelar.Location = new System.Drawing.Point(223, 315);
+            this.btCancelar.Name = "btCancelar";
+            this.btCancelar.Size = new System.Drawing.Size(91, 46);
+            this.btCancelar.TabIndex = 12;
+            this.btCancelar.Text = "Cancelar";
+            this.btCancelar.UseVisualStyleBackColor = true;
+            this.btCancelar.Click += new System.EventHandler(this.btCancelar_Click);
+            // 
+            // btBorrar
+            // 
+            this.btBorrar.Location = new System.Drawing.Point(125, 315);
+            this.btBorrar.Name = "btBorrar";
+            this.btBorrar.Size = new System.Drawing.Size(91, 46);
+            this.btBorrar.TabIndex = 11;
+            this.btBorrar.Text = "Borrar";
+            this.btBorrar.UseVisualStyleBackColor = true;
+            this.btBorrar.Click += new System.EventHandler(this.btBorrar_Click);
+            // 
+            // btGuardar
+            // 
+            this.btGuardar.Location = new System.Drawing.Point(20, 315);
+            this.btGuardar.Name = "btGuardar";
+            this.btGuardar.Size = new System.Drawing.Size(97, 46);
+            this.btGuardar.TabIndex = 10;
+            this.btGuardar.Text = "Guardar/Ingresar";
+            this.btGuardar.UseVisualStyleBackColor = true;
+            this.btGuardar.Click += new System.EventHandler(this.btGuardar_Click);
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(66, 198);
+            this.txtDireccion.Multiline = true;
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(234, 79);
+            this.txtDireccion.TabIndex = 9;
+            // 
+            // txtDistrito
+            // 
+            this.txtDistrito.Location = new System.Drawing.Point(65, 152);
+            this.txtDistrito.Name = "txtDistrito";
+            this.txtDistrito.Size = new System.Drawing.Size(234, 20);
+            this.txtDistrito.TabIndex = 8;
+            // 
+            // txtSueldo
+            // 
+            this.txtSueldo.Location = new System.Drawing.Point(62, 113);
+            this.txtSueldo.Name = "txtSueldo";
+            this.txtSueldo.Size = new System.Drawing.Size(234, 20);
+            this.txtSueldo.TabIndex = 7;
+            // 
+            // txtDni
+            // 
+            this.txtDni.Location = new System.Drawing.Point(64, 70);
+            this.txtDni.MaxLength = 8;
+            this.txtDni.Name = "txtDni";
+            this.txtDni.Size = new System.Drawing.Size(234, 20);
+            this.txtDni.TabIndex = 6;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(63, 31);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(234, 20);
+            this.txtNombre.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 198);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Dirección";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 154);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Distrito";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Sueldo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Dni";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // btBusqueda
+            // 
+            this.btBusqueda.Location = new System.Drawing.Point(264, 407);
+            this.btBusqueda.Name = "btBusqueda";
+            this.btBusqueda.Size = new System.Drawing.Size(75, 23);
+            this.btBusqueda.TabIndex = 2;
+            this.btBusqueda.Text = "Buscar";
+            this.btBusqueda.UseVisualStyleBackColor = true;
+            this.btBusqueda.Click += new System.EventHandler(this.btBusqueda_Click);
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(92, 409);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(153, 20);
+            this.txtBuscar.TabIndex = 3;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(10, 407);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(76, 26);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Busqueda por \r\nnombre\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(927, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtBuscar);
+            this.Controls.Add(this.btBusqueda);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idEmpleado;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Dni;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Distrito;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Direccion;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Sueldo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CreatedAt;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btCancelar;
+        private System.Windows.Forms.Button btBorrar;
+        private System.Windows.Forms.Button btGuardar;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.TextBox txtDistrito;
+        private System.Windows.Forms.TextBox txtSueldo;
+        private System.Windows.Forms.TextBox txtDni;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btBusqueda;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using diosmio.Models;
+
+
+namespace diosmio
+{
+    public partial class Form1 : Form
+    {
+
+        db_lp3_G1Entities db = new db_lp3_G1Entities(); 
+
+        Empleados objEmpleados = new Empleados();
+
+        int idEmp;
+
+        public Form1()
+        {
+            InitializeComponent();
+            MostrarContenido();
+        }
+
+        private void MostrarContenido(string busqueda = null)
+        {
+            dataGridView1.AutoGenerateColumns = false;
+            
+
+
+            if(busqueda != null && !busqueda.Equals(""))
+            {
+                var Empleadosfiltrados = db.Empleados.Where(b => b.Nombre == busqueda).ToList();
+
+                if(Empleadosfiltrados.Count > 0)
+                {
+                    dataGridView1.DataSource = Empleadosfiltrados;
+                }
+                else
+                {
+                    MessageBox.Show("nO se encontro nombre", "aviso", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                }
+
+               
+
+
+
+            }
+            else
+            {
+                dataGridView1.DataSource = db.Empleados.ToList();
+
+               
+
+            }
+
+
+        }
+
+        private void btBusqueda_Click(object sender, EventArgs e)
+        {
+            if (!txtBuscar.Text.Equals(""))
+            {
+                MostrarContenido(txtBuscar.Text.Trim());
+            }
+            else
+            {
+                
+                MostrarContenido();
+                MessageBox.Show("Ingrese nombre","aviso",MessageBoxButtons.OK,MessageBoxIcon.Hand);
+            }
+        }
+
+        private void Actualizar_O_Guardar_Dato()
+        {
+            string dni = txtDni.Text.Trim();
+            string nombre = txtNombre.Text.Trim();
+            string sueldo = txtSueldo.Text.Trim();
+
+            try
+            {
+
+                if (dni.Length >= 8 && nombre.Length > 0 && decimal.Parse(sueldo) > 0)
+                {
+                    objEmpleados.Dni = dni;
+                    objEmpleados.Nombre = nombre;
+                    objEmpleados.Sueldo = Convert.ToDecimal(sueldo);
+                    objEmpleados.Distrito = txtDistrito.Text.Trim();
+                    objEmpleados.Direccion = txtDireccion.Text.Trim();
+                    if (idEmp > 0 )
+                    {
+                        db.Entry(objEmpleados).State = System.Data.Entity.EntityState.Modified;
+                    }
+                    else
+                    {
+                        db.Empleados.Add(objEmpleados);
+                    }
+
+                    db.SaveChanges();
+                    MostrarContenido();
+                    Limpiar();
+
+                }
+                else
+                {
+                    throw new Exception("Falta agregar datos obligatorios.... puto");
+                }
+
+            }
+            catch(Exception ex) { MessageBox.Show(ex.Message); }
+
+        }
+
+
+        private void Eliminar()
+        {
+            try
+            {
+                var respuesta = MessageBox.Show("seguro querés eliminar a este pibe?", "Confirmación", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+                if(respuesta == DialogResult.Yes)
+                {
+                    db.Empleados.Remove(objEmpleados); //Lo removemos del contexto
+                    db.SaveChanges();
+
+                    MostrarContenido();
+                    Limpiar();
+
+                }
+                else
+                {
+                    MessageBox.Show("Cuidando mano, para la proxima publico tus datos personales :v");
+                }
+
+
+            }
+            catch(Exception ex) { MessageBox.Show(ex.Message); }
+        }
+
+        private void Limpiar()
+        {
+            txtDireccion.Clear();
+            txtDistrito.Clear();
+            txtNombre.Clear();
+            txtDni.Clear();
+            txtSueldo.Clear();
+            btBorrar.Enabled = false;
+            idEmp = 0;
+            txtDni.Focus();
+
+        }
+
+
+
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MostrarContenido();
+            Limpiar();
+            
+        }
+
+        private void dataGridView1_DoubleClick(object sender, EventArgs e)
+        {
+            try
+            {
+                if(dataGridView1.CurrentCell.RowIndex != -1)
+                {
+
+                    idEmp = Convert.ToInt32(dataGridView1.CurrentRow.Cells["IdEmpleado"].Value);
+                    objEmpleados = db.Empleados.Where(x => x.idEmpleado == idEmp).FirstOrDefault();
+                    txtDni.Text = objEmpleados.Dni;
+                    txtNombre.Text = objEmpleados.Nombre;
+                    txtSueldo.Text = objEmpleados.Sueldo.ToString();
+                    txtDistrito.Text = objEmpleados.Distrito;
+                    txtDireccion.Text = objEmpleados.Direccion;
+
+                    btBorrar.Enabled = true;
+
+                }
+                
+
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+
+        private void btGuardar_Click(object sender, EventArgs e)
+        {
+            Actualizar_O_Guardar_Dato();
+        }
+
+        private void btBorrar_Click(object sender, EventArgs e)
+        {
+            Eliminar();
+        }
+
+        private void btCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        
+    }
+}
